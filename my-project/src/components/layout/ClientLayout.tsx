b488c6c49f9d0cb409f61fd1033cf990ff87a9e2
StaRCss <staciekon@gmail.com>
2025-05-05 21:44:51 +0300

    Refactor: Finished refactoring and tested everything

---
+++ my-project/src/components/layout/ClientLayout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import Navbar from "./Navbar"; // Import the Navbar normally
+
+const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {/* Hide Navbar on Add Pet page */}
+      {pathname !== "/Pets/Add" && <Navbar />}
+      {children}
+    </>
+  );
+};
+
+export default ClientLayout;
