607bc481fbaee03c81578e2991750e9c824ae810
StaRCss <staciekon@gmail.com>
2025-07-16 21:40:34 +0300

    fix: add useEffect with reset() to apply prop-based default name and imporoved styling

--- my-project/src/components/pets/EditBasicModal.tsx
+++ my-project/src/components/pets/EditBasicModal.tsx
@@ -1,39 +1,61 @@
+import { useForm, FormProvider } from "react-hook-form";
+import { useEffect } from "react";
 import NameInputField from "./fields/NameInputField";
-import {useForm, FormProvider} from "react-hook-form"
 import { CircleX } from "lucide-react";
+import type { PetFormValues } from "./AddPetForm";
 
 type EditBasicModalProps = {
-    isOpen: boolean;
-    onClose: () => void ;
-    id: string;
-    name: string;
-    breed?: string | null;
-    gender?: string | null;
-    birthday?: Date;
+  isOpen: boolean;
+  onClose: () => void;
+  id: string;
+  name: string;
+  breed?: string | null;
+  gender?: string | null;
+  birthday?: Date;
+};
 
-}
+export default function EditBasicModal({
+  isOpen,
+  onClose,
+  id,
+  name,
+  breed,
+  gender,
+  birthday,
+}: EditBasicModalProps) {
+  const methods = useForm<PetFormValues>({
+    defaultValues: {
+      name: "",
+    },
+  });
 
+  useEffect(() => {
+    if (isOpen) {
+      methods.reset({ name });
+    }
+  }, [isOpen, name, methods]);
 
+  if (!isOpen) return null;
 
-export default function EditBasicModal ({isOpen, onClose, id, name , breed, gender, birthday} : EditBasicModalProps) {
-    const methods = useForm();
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white rounded-lg p-6 max-w-md w-full relative">
+        <button
+          className="absolute top-4 right-4 text-purple-500 hover:text-purple-700"
+          onClick={onClose}
+          aria-label="Close Form"
+        >
+          <CircleX />
+        </button>
 
-    if (!isOpen) return null; 
-
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white rounded-lg p-6 max-w-md w-full relative">
-                <button
-               className="absolute top-4 right-4 text-purple-500 hover:text-purple-700"
-               onClick = { (onClose)}
-                >
-              <CircleX />
-
-                </button>
-                <FormProvider {...methods}>
-                    <NameInputField/>
-                </FormProvider>
+        <FormProvider {...methods}>
+          <form>
+            <div className="flex flex-col items-center border-t border-b border-gray-300 gap-4 pb-4 mt-8">
+              <NameInputField />
             </div>
-        </div>
-    );
+          </form>
+        </FormProvider>
+      </div>
+    </div>
+  );
 }
