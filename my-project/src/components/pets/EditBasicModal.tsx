4ca01d1cab3d9610e38a84217dc2115aee6615c2
StaRCss <staciekon@gmail.com>
2025-07-16 21:09:20 +0300

    add onClose button , NameInputField and import react-hook-form

--- my-project/src/components/pets/EditBasicModal.tsx
+++ my-project/src/components/pets/EditBasicModal.tsx
@@ -1,4 +1,6 @@
-
+import NameInputField from "./fields/NameInputField";
+import {useForm, FormProvider} from "react-hook-form"
+import { CircleX } from "lucide-react";
 
 type EditBasicModalProps = {
     isOpen: boolean;
@@ -12,13 +14,25 @@ type EditBasicModalProps = {
 }
 
 
+
 export default function EditBasicModal ({isOpen, onClose, id, name , breed, gender, birthday} : EditBasicModalProps) {
-    if (!isOpen) return null; // <-- this is the fix
+    const methods = useForm();
+
+    if (!isOpen) return null; 
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
             <div className="bg-white rounded-lg p-6 max-w-md w-full relative">
+                <button
+               className="absolute top-4 right-4 text-purple-500 hover:text-purple-700"
+               onClick = { (onClose)}
+                >
+              <CircleX />
 
+                </button>
+                <FormProvider {...methods}>
+                    <NameInputField/>
+                </FormProvider>
             </div>
         </div>
     );
