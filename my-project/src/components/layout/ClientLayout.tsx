2ff797db3348c002880cec428335e587814e63dd
StaRCss <staciekon@gmail.com>
2025-08-18 21:35:48 +0300

    Remove Navbarmenu from weight-history route

--- my-project/src/components/layout/ClientLayout.tsx
+++ my-project/src/components/layout/ClientLayout.tsx
@@ -2,16 +2,18 @@
 
 import React from "react";
 import { usePathname } from "next/navigation";
-import Navbar from "./Navbar"; // Import the Navbar normally
+import Navbar from "./Navbar";
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
+  const isAddPetPage = pathname === "/dashboard/pets/Add";
+  const isWeightHistoryPage = /^\/dashboard\/pets\/[^/]+\/weight-history$/.test(pathname);
+  // Matches /dashboard/pets/{id}/weight-history
+
   return (
     <>
-      {/* Hide Navbar on Add Pet page */}
-      {pathname !== "/dashboard/pets/Add"  && <Navbar />}
-      
+      {!isAddPetPage && !isWeightHistoryPage && <Navbar />}
       {children}
     </>
   );
