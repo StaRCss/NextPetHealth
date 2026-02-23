b7a167e322ecd0321a4c2287fc3db62fb4e5145f
StaRCss <staciekon@gmail.com>
2026-02-23 22:47:21 +0200

    refactor(providers): remove MantineProvider wrapper from session provider

--- my-project/src/app/providers.tsx
+++ my-project/src/app/providers.tsx
@@ -2,16 +2,11 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
-import { MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
-import '@mantine/dates/styles.css';
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <MantineProvider>
-        {children}
-      </MantineProvider>
+      {children}
     </SessionProvider>
   );
 }
